7ebec1a004ac0bdbf9c1be571fb62742af7cbcb3
dehvCurtis <dehvonc@gmail.com>
2025-11-01 19:45:50 -0600

    chore: add solidity-sbom v0.9.0 formula

---
+++ Formula/solidity-sbom.rb
@@ -0,0 +1,36 @@
+# Homebrew Formula for solidity-sbom
+# To install locally for testing: brew install --build-from-source Formula/solidity-sbom.rb
+
+class SoliditySbom < Formula
+  desc "Software Bill of Materials (SBOM) generator for Solidity smart contracts"
+  homepage "https://github.com/BlockSecOps/SolidityBOM"
+  url "https://github.com/BlockSecOps/SolidityBOM/archive/refs/tags/v0.9.0.tar.gz"
+  sha256 "de043cbf4ad787b0f561a69db46b8c7d7410b2cc0c023eefbdf4d1fbdbeb07c2"
+  license "MIT OR Apache-2.0"
+  head "https://github.com/BlockSecOps/SolidityBOM.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    cd "solidity-sbom" do
+      system "cargo", "install", *std_cargo_args
+    end
+
+    # Install shell completions
+    generate_completions_from_executable(bin/"solidity-sbom", "completions")
+  end
+
+  test do
+    # Verify the binary works
+    assert_match "solidity-sbom", shell_output("#{bin}/solidity-sbom --version")
+
+    # Test analyze command help
+    assert_match "Generate SBOM", shell_output("#{bin}/solidity-sbom analyze --help")
+
+    # Test graph command help
+    assert_match "dependency graph", shell_output("#{bin}/solidity-sbom graph --help")
+
+    # Test init command help
+    assert_match "configuration", shell_output("#{bin}/solidity-sbom init --help")
+  end
+end
