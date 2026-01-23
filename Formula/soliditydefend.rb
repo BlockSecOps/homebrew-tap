14ae3c2bd6cf531a4a207d0453781634dd1d7b2a
dehvCurtis <dehvonc@gmail.com>
2026-01-22 21:05:59 -0700

    chore: update SolidityDefend formula to v1.10.6

--- Formula/soliditydefend.rb
+++ Formula/soliditydefend.rb
@@ -1,21 +1,21 @@
 class Soliditydefend < Formula
   desc "High-performance static analysis security tool for Solidity smart contracts"
   homepage "https://github.com/BlockSecOps/SolidityDefend"
-  version "1.10.5"
+  version "1.10.6"
   license "MIT OR Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/BlockSecOps/SolidityDefend/releases/download/v1.10.5/soliditydefend-v1.10.5-darwin-arm64.tar.gz"
-      sha256 "07e458f4268cc8aec7c95a6a9e6a68c2b0a5927f5e8fcc6e8e0c04ec6574fe98"
+      url "https://github.com/BlockSecOps/SolidityDefend/releases/download/v1.10.6/soliditydefend-v1.10.6-darwin-arm64.tar.gz"
+      sha256 "f8b2f5c101972c2fcbf54df2b7bb89c6ffdc6981047827b5a6c8998343f1d401"
     else
-      url "https://github.com/BlockSecOps/SolidityDefend/releases/download/v1.10.5/soliditydefend-v1.10.5-darwin-x86_64.tar.gz"
-      sha256 "1808f1fbeb0e9dc8b67529f294f30a85fe12eeda84e50b1f8ce9ef9644a42537"
+      url "https://github.com/BlockSecOps/SolidityDefend/releases/download/v1.10.6/soliditydefend-v1.10.6-darwin-x86_64.tar.gz"
+      sha256 "REPLACE_WITH_ACTUAL_SHA256_X86_64"
     end
   end
 
   on_linux do
-    url "https://github.com/BlockSecOps/SolidityDefend/releases/download/v1.10.5/soliditydefend-v1.10.5-linux-x86_64.tar.gz"
+    url "https://github.com/BlockSecOps/SolidityDefend/releases/download/v1.10.6/soliditydefend-v1.10.6-linux-x86_64.tar.gz"
     sha256 "REPLACE_WITH_ACTUAL_SHA256_LINUX"
   end
 
